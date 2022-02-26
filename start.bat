c925c9bf2f1028a197ade4c8c6829d9bac80fbec
Johnny Cassidy <51786769+pJanks@users.noreply.github.com>
2022-02-26 12:49:26 -0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,4 @@
-cd path/to/your/project/directory
+# if file is not in current directory uncomment following line and adjust to fit your needs
+# cd path/to/your/project/directory
 start http://127.0.0.1:3001
 node server.js
